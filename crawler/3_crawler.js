b796875d27b0a7a5f56801d3fef597ab947ade43
Yun-Han <yhanh999@gmail.com>
2022-05-27 11:18:45 +0800

    update crawler wrote in async await

--- crawler/3_crawler.js
+++ crawler/3_crawler.js
@@ -2,38 +2,45 @@
 // 1. read stock no from file (fs)
 // 2. axios.get to request data
 
+// read stock no from stock.txt
+
 // npm i axios
 const axios = require("axios");
 const fs = require("fs");
 
-let p = new Promise((resolve, reject) => {
-  fs.readFile("stock.txt", "utf-8", (err, stockNo) => {
-    if (err) {
-      // console.error("read file error", err);
-      reject("read file error", err);
-    } else {
-      // console.log("read stock no from file:", stockNo);
-      resolve("read stock no from file:", stockNo);
-      // https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220301&stockNo=2330
-      axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-        params: {
-          // 設定 query string
-          response: "json",
-          date: "20220301",
-          stockNo: stockNo,
-        },
-      });
+fs.readFile("stock.txt", "utf-8", (err, stockNo) => {
+  if (err) {
+    console.error("read file error", err);
+  } else {
+    console.log("read stock no from file:", stockNo);
+    // https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20220301&stockNo=2330
+
+    async function getData() {
+      try {
+        let response = await axios.get(
+          "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+          {
+            params: {
+              // 設定 query string
+              response: "json",
+              date: "20220301",
+              stockNo: stockNo,
+            },
+          }
+        );
+        console.log(response.data);
+      } catch (e) {
+        console.error(e);
+      }
     }
-  });
-});
 
-async function getData() {
-  try {
-    let response = await p;
-    console.log(response);
-  } catch (e) {
-    console.error(e);
+    // .then((response) => {
+    //   // response 物件
+    //   console.log(response.data);
+    // })
+    // .catch((e) => {
+    //   console.error(e);
+    // });
+    getData();
   }
-}
-
-getData();
+});
