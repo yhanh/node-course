db10b80ffeeccc9ea07901e0fa62d3e32ad1ac0b
Yun-Han <yhanh999@gmail.com>
2022-05-27 14:14:23 +0800

    update server pages

--- simple-express/server.js
+++ simple-express/server.js
@@ -14,6 +14,14 @@ app.get("/about", (request, response, next) => {
   response.send("About Me");
 });
 
+app.get("/products", (request, response, next) => {
+  response.send("There are sooooooo many good products here~");
+});
+
+app.get("/login", (request, response, next) => {
+  response.send("Welcome and have your personal serve!");
+});
+
 app.listen(3001, () => {
   console.log("Server staart at 3001.");
 });
