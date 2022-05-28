f61e333b9b2433c7c2dc3b28ea255436b51f8623
Yun-Han <yhanh999@gmail.com>
2022-05-28 22:46:08 +0800

    explain '__dirname'

--- basic/dir/dir-readfile.js
+++ basic/dir/dir-readfile.js
@@ -5,3 +5,6 @@ const path = require('path');
   let result = await fs.readFile(path.join(__dirname, "test.txt"), "utf-8");
   console.log(result);
 })();
+
+// 本來 readfile 裡的檔案，會根據"人"在哪裡而決定
+// 更好的方法: 用程式碼檔案自己的位置 ==> dir name
