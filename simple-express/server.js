afaaaac53f4d78ff79d8e76bf84f00535ceba73d
Yun-Han <yhanh999@gmail.com>
2022-05-29 00:21:45 +0800

    install cors -- a third-party  middleware which permit front end to access back end datas

--- simple-express/server.js
+++ simple-express/server.js
@@ -6,6 +6,10 @@ const app = express();
 
 const path = require('path');
 
+// 使用第三方開發的中間件 cors
+const cors = require('cors');
+app.use(cors());
+
 const mysql = require('mysql2');
 require('dotenv').config();
 
@@ -81,6 +85,7 @@ app.get("/error", (req, res, next) => {
 
 // stocks
 app.get('/stocks',async (req, res, next) => {
+  console.log("我是股票列表");
   let [data, fields] = await pool.execute("SELECT * FROM stocks");
   res.json(data);
 });
