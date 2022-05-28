69cb3760df73e7c0e3aa51a6ee3a1084f8605f88
Yun-Han <yhanh999@gmail.com>
2022-05-29 00:15:03 +0800

    font-end stock example file

---
+++ stock-fe/src/components/Stock.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const Stock = () => {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    let getStocks = async () => {
+      let response = await axios.get('http://localhost:3001/stocks');
+      setStocks(response.data);
+    };
+    getStocks();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
+
+      {stocks.map((stock) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/${stock.id}`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Stock;
